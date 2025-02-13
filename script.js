ac228bb865ef8bfd0ff7c4ffd372f1a876b13e71
mate2hais <matei.adrian694@yahoo.com>
2025-02-13 13:30:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let arrayWords = [];
+const arrayWords = [];
 
 function saveWord() {
-  let name = document.getElementById('inputVal').value
+  let name = document.getElementById('inputVal').value;
   arrayWords.push(name);
 }
 
 function searchWord() {
-  let word = document.getElementById('inputVal').value
-  for(i in arrayWords){
-    if(word === arrayWords[i]){
-      document.getElementById('wordRevealed').innerHTML = word + " is in the dictionary"
-       break;
-    }else{
+  let word = document.getElementById('inputVal').value;
+  for (let i = 0; i < arrayWords.length; ++i) {
+    if (word === arrayWords[i]) {
+      document.getElementById('wordRevealed').innerHTML = word + " is in the dictionary";
+      break;
+    } else {
       document.getElementById('wordRevealed').innerHTML = "This word does not exist in the dictionary";
     }
   }
